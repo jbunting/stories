27c863de3e81a3ea786804de8c3482e2eda5f400
Jacques Woodcock <jacques@kitewebconsulting.com>
2014-06-01 10:08:31 -0500

    Adding in functionality for swaping views.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+var currentView = 'mapDiv'; // the current view
+
 function storyApp() {
 
     // Get content
@@ -6,10 +8,28 @@ function storyApp() {
 
     // Setup accordian
     accordion.start( $('#intake_wrapper'), 'step_header', 'step_content' );
+}
 
+function swapView( target ) {
+    if ( currentView == target ) {
+        return false;
+    }
+    // hide the current view
+    if (currentView) {
+        $( '#' + currentView ).hide( 200 );
+    }
+    // how show current
+    $( '#' + target ).show( 200 );
+    // update active
+    currentView = target;
 }
 
 $( document ).ready( function() {
     // start app
     storyApp();
+
+    $( '.navBut' ).on( 'click', function() {
+        var tar = $( this ).data( 'key' );
+        swapView( tar );
+    });
 });
